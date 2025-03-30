c92e8de055bb848e76f469b5dbb39ed360e8ed8f
Devalo1 <dani_popa21@yahoo.ro>
2025-03-31 01:03:20 +0300

    Adăugat build-netlify script și configurare Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +13,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
