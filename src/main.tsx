3b52f68c2e3c795825f53d961edb24b4d2e9db56
Devalo1 <dani_popa21@yahoo.ro>
2025-03-30 16:23:07 +0300

    Descrierea modificărilor

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,19 @@ import App from './App';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 
-// Setare variabilă CSS pentru base path
-const setBasePath = () => {
+// Setare căi imagini în funcție de mediu
+const setImagePaths = () => {
   // Detectează dacă este pe GitHub Pages sau local
   const isGitHubPages = window.location.hostname === 'devalo1.github.io';
   const basePath = isGitHubPages ? '/my-website' : '';
   
-  // Setează variabila CSS
-  document.documentElement.style.setProperty('--base-url', basePath);
+  // Setează variabilele CSS pentru căile complete
+  document.documentElement.style.setProperty('--background-url', `${basePath}/background.jpg`);
+  document.documentElement.style.setProperty('--cover-url', `${basePath}/images/cover.jpeg`);
 };
 
-// Apelează funcția pentru a seta calea de bază
-setBasePath();
+// Apelează funcția pentru a seta căile
+setImagePaths();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
